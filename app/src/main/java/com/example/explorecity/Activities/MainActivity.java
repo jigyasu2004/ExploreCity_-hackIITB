55896b632a9e52fdb836b8c83379b47e347e85ef
jigyasu2004 <22cs3031@rgipt.ac.in>
2023-12-06 01:06:56 +0530

    This is Explore City.

--- app/src/main/java/com/example/explorecity/Activities/MainActivity.java
+++ app/src/main/java/com/example/explorecity/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.explorecity;
+package com.example.explorecity.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.explorecity.R;
 
 public class MainActivity extends AppCompatActivity {
 
